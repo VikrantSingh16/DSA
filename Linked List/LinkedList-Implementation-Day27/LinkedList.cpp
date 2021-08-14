1132f23bb844190f467532a0960479a4a963a819
Vikrant Singh <66239767+VikrantSingh16@users.noreply.github.com>
2021-08-14 09:55:12 +0530

    Add files via upload

---
+++ Linked List/LinkedList-Implementation-Day27/LinkedList.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class node{
+       public:
+    int data;
+    node *next;
+node(int val){
+data=val;
+next=NULL;
+}
+};
+void insertAtEnd(node* &head,int val){
+    node* n = new node(val);
+       node* temp = head;
+
+    if(head==NULL){
+        head=n;
+    }
+    while(temp->next!=NULL){
+        temp=temp->next;
+}
+temp->next=n;
+}
+void display(node* head){
+node* temp=head;
+while(temp!=NULL){
+    cout<<temp->data<<"->";
+    temp=temp->next;
+}
+}
+int main()
+{
+ node* head=NULL;
+ insertAtEnd(head,1);
+  insertAtEnd(head,2);
+insertAtEnd(head,3);
+display(head);
+ return 0;
+}
